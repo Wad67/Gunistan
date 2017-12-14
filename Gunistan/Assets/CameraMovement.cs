9558f82bf3f45ed210ffc8433be946d999f9681a
Wad67 <Wad67@live.com.au>
2017-12-15 01:09:52 +1000

    Initial

---
+++ Gunistan/Assets/CameraMovement.cs
@@ -0,0 +1,66 @@
+﻿// Credit to damien_oconnell from http://forum.unity3d.com/threads/39513-Click-drag-camera-movement
+// for using the mouse displacement for calculating the amount of camera movement and panning code.
+
+using UnityEngine;
+using System.Collections;
+
+
+
+	
+
+
+public class CameraMovement : MonoBehaviour 
+{
+	//
+	// VARIABLES
+	//
+
+
+
+
+
+	private Vector3 panOrigin;	// Position of cursor when mouse dragging starts
+	private Vector3 oldPos;
+	public Camera cam;
+	private float orthoZoom;
+	//
+	// UPDATE
+	//
+
+	void Update () 
+	{
+		// Get the right mouse button
+		if(Input.GetMouseButtonDown(0))
+		{
+			oldPos = transform.position;
+			panOrigin = Camera.main.ScreenToViewportPoint(Input.mousePosition);                    //Get the ScreenVector the mouse clicked
+		}
+
+		if(Input.GetMouseButton(0))
+		{
+			Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition) - panOrigin;    //Get the difference between where the mouse clicked and where it moved
+			pos = pos * 25; //It's slow as fuck for some reason so just multiply it 
+			transform.position = oldPos + -pos;                                         //Move the position of the camera to simulate a drag, speed * 10 for screen to worldspace conversion
+		}
+
+
+		// Zooming
+		var d = Input.GetAxis("Mouse ScrollWheel");
+		if (d > 0f)
+		{
+			cam.orthographicSize--;
+		}
+		else if (d < 0f)
+		{
+			cam.orthographicSize++;
+		}
+		orthoZoom = cam.orthographicSize;
+
+
+	
+
+
+
+
+	}
+}
---
+++ Gunistan/Assets/MainMenu.cs
@@ -0,0 +1,30 @@
+﻿//C#
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	public int startX,startY;
+	public bool center;
+
+	void OnGUI () {
+		if (center) {
+			startX = Screen.width / 2;
+			startY = Screen.height / 2;
+		}
+		// Make a background box
+		GUI.Box(new Rect(startX,startY,100,90), "Gunistan");
+
+		// Make the first button. If it is pressed, Application.Loadlevel (1) will be executed
+		if(GUI.Button(new Rect(startX + 10,startY + 30,80,20), "New Game")) {
+			SceneManager.LoadScene ("new");
+		}
+
+		// Make the second button.
+		//if(GUI.Button(new Rect(20,70,80,20), "Level 2")) {
+		//	Application.LoadLevel(2);
+		//}
+	}
+}
+
